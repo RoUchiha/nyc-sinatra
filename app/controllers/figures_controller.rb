4405d53cf19320c262e18aabfa17fef79e0083a2
RoUchiha <swaggsikh@gmail.com>
2021-11-17 19:28:16 -0600

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,66 @@
 class FiguresController < ApplicationController
   # add controller methods
+
+  get '/figures' do
+    @figures = Figure.all
+    erb :'/figures/index'
+  end
+
+  get '/figures/new' do
+    @landmarks = Landmark.all
+    @titles = Title.all
+    erb :'/figures/new'
+  end
+
+  post '/figures' do
+    @figure = Figure.create(params[:figure])
+    
+
+    if !params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.create(name: params[:landmark][:name], year_completed: params[:landmark][:year_completed])
+    end
+
+    if !params[:title][:name].empty?
+      @figure.titles << Title.create(name: params[:title][:name])
+    end
+
+    @figure.save
+
+    redirect "figures/#{@figure.id}"
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb :'figures/show'
+  end
+
+  get '/figures/:id/edit' do
+    @landmarks = Landmark.all
+    @titles = Title.all
+    @figure = Figure.find(params[:id])
+    erb :'/figures/edit'
+  end
+
+  patch '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    
+    if !params[:figure].keys.include?("landmark_ids")
+      params[:figure]["landmark_ids"] = []
+    end
+    if !params[:figure].keys.include?("title_ids")
+      params[:figure]["title_ids"] = []
+    end
+
+    @figure.update(params[:figure])
+    
+    if !params[:figure][:name].empty?
+      @figure.figure = Figure.create(params[:figure])
+    end
+
+    @figure.save
+   
+    redirect "figures/#{@figure.id}"
+  end
+
+
 end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,52 @@
 class LandmarksController < ApplicationController
   # add controller methods
+
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    erb :'/landmarks/index'
+  end
+
+  get '/landmarks/new' do
+    @figures = Figure.all
+    erb :'/landmarks/new'
+  end
+
+  post '/landmarks' do
+    @landmark = Landmark.create(name: params[:landmark][:name], year_completed: params[:landmark][:year_completed])
+    if !params[:figure][:name].empty?
+      @landmark.figure = Figure.create(name: params[:figure][:name])
+    end
+    @landmark.save
+    redirect "landmarks/#{@landmark.id}"
+  end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    erb :'landmarks/show'
+  end
+
+  get '/landmarks/:id/edit' do
+    @figures = Figure.all
+    @landmark = Landmark.find(params[:id])
+    erb :'/landmarks/edit'
+  end
+
+  patch '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    
+    if !params[:landmark].include?("figure_id")
+      params[:landmark]["figure_id"] = []
+    end
+
+    @landmark.update(name: params[:landmark][:name], year_completed: params[:landmark][:year_completed])
+    
+    if !params[:figure][:name].empty?
+      @landmark.figure = Figure.create(name: params[:figure][:name])
+    end
+
+    @landmark.save
+   
+    redirect "landmarks/#{@landmark.id}"
+  end
+
 end
